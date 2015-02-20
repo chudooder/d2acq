8ef47a21c51aacee0bd3e6be34003ba6fe2fd92a
chudooder <codeshawnachu@gmail.com>
2015-02-20 14:27:34 -0600

    add score popup

--- js/player.js
+++ js/player.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 	var player = $('#ability-player');
 	player[0].volume = 0.5;
-	console.log(player)
 	$('#ability-icon').click(function(){
 		player.currentTime = 0;
 		player[0].play();
