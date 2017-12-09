6fd11a4e8b58ed7d20c28db2da24f12ba67a0332
chudooder <codeshawnachu@gmail.com>
2017-12-08 19:33:03 -0600

    Update to 7.07

--- genAbilityList.py
+++ genAbilityList.py
@@ -2,7 +2,7 @@ import os
 
 out = open('abilities.txt', 'w')
 
-for f in os.listdir(os.getcwd()+'/icons'):
-	out.write(f+'\n')
+for f in os.listdir(os.getcwd()+'/img/icons'):
+    out.write(os.path.splitext(f)[0]+'\n')
 
 out.close()
--- soundGrabber.py
+++ soundGrabber.py
@@ -1,49 +1,49 @@
 from bs4 import BeautifulSoup
 import mechanize
 import re
-import urllib	
+import urllib    
+from hashlib import sha256
 
 br = mechanize.Browser()
 br.set_handle_robots(False)
 br.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1) \
-                    AppleWebKit/537.36 (KHTML, like Gecko) \
-                    Chrome/41.0.2228.0 Safari/537.36')]
+    AppleWebKit/537.36 (KHTML, like Gecko) \
+    Chrome/41.0.2228.0 Safari/537.36')]
 
 def parseHero(heroLink):
-	html = br.open(heroLink)
-	soup = BeautifulSoup(html.read())
-	abilities = soup.find_all('table', class_ = 'abilitytable')
-	for ability in abilities:
-		# get the ability sound file
-		btn = ability.find('a', title='Play', class_='sm2_button')
-		if btn is None:
-			continue
-		sndlink = btn.attrs['href']
+    html = br.open(heroLink)
+    soup = BeautifulSoup(html.read(), 'lxml')
+    abilities = soup.find_all(lambda elem: elem.name == 'div' and 'style' in elem.attrs and 'flex: 0 1 450px' in elem.attrs['style'])
+    print '%d abilities found' % (len(abilities),)
+    for ability in abilities:
+        # get the ability sound file
+        btn = ability.find('a', title='Play', class_='sm2_button')
+        if btn is None:
+            continue
+        sndlink = btn.attrs['href']
 
-		# get the ability icon
-		icon = ability.find('img', width=128)
-		iconlink = icon.attrs['src']
+        # get the ability icon
+        icon = ability.find('img', width=128)
+        iconlink = icon.attrs['src']
 
-		# get the name of the ability
-		match = re.search(r'/([\w\d%_-]+)\.mp3$', sndlink)
-		name = re.sub('_', ' ', urllib.unquote(match.group(1)).encode('ascii', 'ignore'))
+        # get the name of the ability
+        name = ability.div.get_text('|').split('|')[0]
 
-		print name
+        if name == 'Cleave':
+            continue
+        if name == 'Aegis of the Immortal':
+            continue
 
-		dl = urllib.URLopener()
-		dl.retrieve(sndlink, 'sounds/'+name+'.mp3')
-		dl.retrieve(iconlink, 'icons/'+name+'.png')
+        print name
 
+        dl = urllib.URLopener()
+        dl.retrieve(sndlink, 'audio/'+sha256(name.encode()).hexdigest() +'.mp3')
+        dl.retrieve(iconlink, 'img/icons/'+name+'.png')
 
 html = br.open('http://dota2.gamepedia.com/Heroes')
-soup = BeautifulSoup(html.read())
+soup = BeautifulSoup(html.read(), 'lxml')
 heroes = soup.find_all('img', width=80, height=45)
-start = False
 for hero in heroes:
-	link = 'http://dota2.gamepedia.com/'+re.sub(' ', '_', hero.attrs['alt'])
-	print link
-	if 'Tinker' in link:
-		start = True
-
-	if start == True:
-		parseHero(link)
+    link = 'http://dota2.gamepedia.com' + hero.parent.attrs['href']
+    print link
+    parseHero(link)
